29ee5f0edc4d6654be7da52ec6ad462b78aef884
DanilAnufriev <danilpetr2040@gmail.com>
2023-07-09 14:50:19 +0600

    the second task

--- задачи.js
+++ задачи.js
@@ -6,3 +6,17 @@ console.log(palindrome('racecar'))
 console.log(palindrome('А роза упала на лапу Азора'))
 
 
+function FizzBuzz(num) {
+    for (let i = 0; i <= num; i++) {
+        if(i % 3 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if(i % 3 === 0) {
+            console.log('fizz')
+        } else if(i % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+FizzBuzz(30)
